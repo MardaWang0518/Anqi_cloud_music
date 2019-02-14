23093a53e4a4fe765ac9c09930c5c6bc96ce7f22
wangy@workai.com.cn <wangy@workai.com.cn>
2019-02-14 10:19:39 +0800

    yh

--- utils/fav.js
+++ utils/fav.js
@@ -3,10 +3,7 @@ module.exports = {
 		var favList = [];
 		var data = wx.getStorageSync('favlist');
 
-<<<<<<< HEAD
     console.log("getFavList---",data);
-=======
->>>>>>> eaf734405bc199f062d56a25c5fa8af89491bd9a
 		Object.keys(data).forEach(function(key) {
 			favList.push({
 				picurl: data[key].picurl,
